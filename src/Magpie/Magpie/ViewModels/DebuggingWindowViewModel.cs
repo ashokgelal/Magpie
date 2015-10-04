a4b89299ab5f0fa43be4089e91eea945d253742a
Ashok Gelal <ashok@metageek.net>
2015-10-04 15:48:04 -0600

    Add a delegate command and bind window buttons to corresponding commands

--- src/Magpie/Magpie/ViewModels/DebuggingWindowViewModel.cs
+++ src/Magpie/Magpie/ViewModels/DebuggingWindowViewModel.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Diagnostics;
-using System.Net;
+﻿using System.Diagnostics;
 using System.Security.Policy;
 using Magpie.Interfaces;
-using Magpie.Models;
 
 namespace Magpie.ViewModels
 {
@@ -12,40 +9,6 @@ namespace Magpie.ViewModels
         public void Log(string message)
         {
             Trace.TraceInformation("Magpie: {0}", message);
-        }
-    }
-
-    internal class MainWindowViewModel : BindableBase
-    {
-        private readonly RemoteAppcast _remoteAppcast;
-        private readonly IDebuggingInfoLogger _logger;
-        private string _releaseNotes;
-
-        public string ReleaseNotes
-        {
-            get { return _releaseNotes; }
-            set
-            {
-                SetProperty(ref _releaseNotes, value);
-            }
-        }
-
-        public MainWindowViewModel(RemoteAppcast appcast, IDebuggingInfoLogger logger)
-        {
-            _remoteAppcast = appcast;
-            _logger = logger;
-            PrepareReleaseNotes(appcast.ReleaseNotesUrl);
-        }
-
-        private async void PrepareReleaseNotes(string releaseNotesUrl)
-        {
-            _logger.Log("Fetching release notes");
-            var client = new WebClient();
-            var notes = await client.DownloadStringTaskAsync(new Uri(releaseNotesUrl)).ConfigureAwait(false);
-            _logger.Log("Finished fetching release notes");
-            _logger.Log("Converting release notes from markdown to html");
-            ReleaseNotes = CommonMark.CommonMarkConverter.Convert(notes);
-            _logger.Log("Finished converting release notes from markdown to html");
         }
     }
 }
---
+++ src/Magpie/Magpie/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Windows.Input;
+using Magpie.Interfaces;
+using Magpie.Models;
+
+namespace Magpie.ViewModels
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+        private readonly RemoteAppcast _remoteAppcast;
+        private readonly IDebuggingInfoLogger _logger;
+        private string _releaseNotes;
+
+        public string ReleaseNotes
+        {
+            get { return _releaseNotes; }
+            set
+            {
+                SetProperty(ref _releaseNotes, value);
+            }
+        }
+
+        public ICommand ContinueUpdateCommand { get; set; }
+        public ICommand SkipThisVersionCommand { get; set; }
+        public ICommand RemindMeLaterCommand { get; set; }
+
+        public MainWindowViewModel(RemoteAppcast appcast, IDebuggingInfoLogger logger)
+        {
+            _remoteAppcast = appcast;
+            _logger = logger;
+            InitializeCommands();
+            PrepareReleaseNotes(appcast.ReleaseNotesUrl);
+        }
+
+        private void InitializeCommands()
+        {
+            ContinueUpdateCommand = new DelegateCommand(ContinueUpdateCommandHandler);
+            SkipThisVersionCommand = new DelegateCommand(SkipThisVersionCommandHandler);
+            RemindMeLaterCommand = new DelegateCommand(RemindMeLaterCommandHandler);
+        }
+
+        private void RemindMeLaterCommandHandler(object obj)
+        {
+            _logger.Log("Remind me later command invoked");
+        }
+
+        private void SkipThisVersionCommandHandler(object obj)
+        {
+            _logger.Log("Skip this version command invoked");
+        }
+
+        private void ContinueUpdateCommandHandler(object obj)
+        {
+            _logger.Log("Continue with update command invoked");
+        }
+
+        private async void PrepareReleaseNotes(string releaseNotesUrl)
+        {
+            _logger.Log("Fetching release notes");
+            var client = new WebClient();
+            var notes = await client.DownloadStringTaskAsync(new Uri(releaseNotesUrl)).ConfigureAwait(false);
+            _logger.Log("Finished fetching release notes");
+            _logger.Log("Converting release notes from markdown to html");
+            ReleaseNotes = CommonMark.CommonMarkConverter.Convert(notes);
+            _logger.Log("Finished converting release notes from markdown to html");
+        }
+    }
+}
