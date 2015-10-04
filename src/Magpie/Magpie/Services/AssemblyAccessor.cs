9b2a7802ed5f8d0cb824bb8536fedf2cdd948250
Ashok Gelal <ashok@metageek.net>
2015-10-04 17:38:25 -0600

    Add AssemblyAccessor to access different attributes of the entry assembly.

---
+++ src/Magpie/Magpie/Services/AssemblyAccessor.cs
@@ -0,0 +1,58 @@
+using System.IO;
+using System.Reflection;
+
+namespace Magpie.Services
+{
+    internal class AssemblyAccessor
+    {
+        private readonly Assembly _assembly;
+
+        internal AssemblyAccessor()
+        {
+            _assembly = Assembly.GetEntryAssembly();
+        }
+
+        internal string Title
+        {
+            get
+            {
+                var attributes = _assembly.GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    var titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (!string.IsNullOrEmpty(titleAttribute.Title))
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        internal string Version
+        {
+            get
+            {
+                return _assembly.GetName().Version.ToString();
+            }
+        }
+
+        internal string Product
+        {
+            get
+            {
+                var attributes = _assembly.GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                return attributes.Length == 0 ? string.Empty : ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        internal string Company
+        {
+            get
+            {
+                var attributes = _assembly.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                return attributes.Length == 0 ? string.Empty : ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+    }
+}
---
+++ src/Magpie/Magpie/Services/MagicStrings.cs
@@ -0,0 +1,10 @@
+namespace Magpie.Services
+{
+    internal class MagicStrings
+    {
+        internal const string SKIP_VERSION_KEY = "SkipVersion";
+        internal const string IS_FIRST_RUN = "IsFirstRun";
+        internal const string LAST_CHECK_DATE = "LastCheckDate";
+        internal const string REG_ROOT_PATH = "Software\\{0}\\{1}\\Magpie";
+    }
+}
