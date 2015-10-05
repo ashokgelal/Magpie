47826de8b4507e08e948967a87186389a40ee1c8
Ashok Gelal <ashokgelal@gmail.com>
2015-10-04 20:57:59 -0600

    Give some styling love

--- src/Magpie/Magpie/Views/MainWindow.xaml.cs
+++ src/Magpie/Magpie/Views/MainWindow.xaml.cs
@@ -1,15 +1,49 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace Magpie.Views
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void PoweredBy_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+
+    public class BrowserBehavior
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+                "Html",
+                typeof(string),
+                typeof(BrowserBehavior),
+                new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser d)
+        {
+            return (string)d.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebBrowser d, string value)
+        {
+            d.SetValue(HtmlProperty, value);
+        }
+
+        static void OnHtmlChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var webBrowser = dependencyObject as WebBrowser;
+            if (webBrowser != null)
+                webBrowser.NavigateToString(e.NewValue as string ?? "&nbsp;");
         }
     }
 }
