faf427da87a21bac200511852ba87e75b6aea6c0
Mike Byce <mike@metageek.net>
2015-12-10 13:25:26 -0700

    Added some XML documentation for the interfaces

--- src/Magpie/Magpie/Interfaces/IDebuggingInfoLogger.cs
+++ src/Magpie/Magpie/Interfaces/IDebuggingInfoLogger.cs
@@ -2,6 +2,10 @@
 {
     public interface IDebuggingInfoLogger
     {
+        /// <summary>
+        /// Logs a string to active logger
+        /// </summary>
+        /// <param name="message">The string to be logged</param>
         void Log(string message);
     }
 }
--- src/Magpie/Magpie/Interfaces/IMagpieService.cs
+++ src/Magpie/Magpie/Interfaces/IMagpieService.cs
@@ -2,5 +2,18 @@ namespace Magpie.Interfaces
 {
     public interface IMagpieService
     {
+        /// <summary>
+        /// Retreives the AppCast data and either shows the UpdateWindow or NoUpdateWindow based on comparison with version and BuildDate of host application
+        /// </summary>
+        /// <param name="appcastUrl">URL for the AppCast file</param>
+        /// <param name="showDebuggingWindow">Does nothing unless you provide an overriden implementation</param>
+        void CheckInBackground(string appcastUrl, bool showDebuggingWindow);
+
+        /// <summary>
+        /// Does exactly the same thing as <see cref="CheckInBackground"/> unless you provide and overriden implementation
+        /// </summary>
+        /// <param name="appcastUrl">URL for the AppCast file</param>
+        /// <param name="showDebuggingWindow">Does nothing unless you provide an overriden implementation</param>
+        void ForceCheckInBackground(string appcastUrl, bool showDebuggingWindow);
     }
 }
