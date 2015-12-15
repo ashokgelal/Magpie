97f465fd82a941e475a93eb8955e92e040ea7854
Ashok Gelal <ashokgelal@gmail.com>
2015-12-15 14:02:15 -0700

    Change embedded styles to default one

--- src/Magpie/Magpie/ViewModels/MainWindowViewModel.cs
+++ src/Magpie/Magpie/ViewModels/MainWindowViewModel.cs
@@ -121,7 +121,7 @@ namespace Magpie.ViewModels
         protected virtual string GetStylesheet()
         {
             var stylesheetStream = Resources.ResourceManager.GetObject("style") as string;
-            return stylesheetStream ?? String.Empty;
+            return stylesheetStream ?? string.Empty;
         }
     }
 }
