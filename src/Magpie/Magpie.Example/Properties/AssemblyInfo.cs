1797267bb2a437f1e317b3742607c2c816fa3f8e
Ashok Gelal <ashokgelal@gmail.com>
2016-01-12 16:17:54 -0700

    Bump version and release it to nuget.org

--- src/Magpie/Magpie.Example/Properties/AssemblyInfo.cs
+++ src/Magpie/Magpie.Example/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MagpieBird, LLC")]
 [assembly: AssemblyProduct("Magpie.Example")]
-[assembly: AssemblyCopyright("Ashok Gelal Copyright ©  2015")]
+[assembly: AssemblyCopyright("Ashok Gelal Copyright ©  2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+[assembly: AssemblyVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.0.2.0")]
--- src/Magpie/Magpie/Properties/AssemblyInfo.cs
+++ src/Magpie/Magpie/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Magpie")]
-[assembly: AssemblyCopyright("Ashok Gelal Copyright ©  2015")]
+[assembly: AssemblyCopyright("Ashok Gelal Copyright ©  2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.7.0")]
-[assembly: AssemblyFileVersion("0.0.7.0")]
+[assembly: AssemblyVersion("0.0.8.0")]
+[assembly: AssemblyFileVersion("0.0.8.0")]
 [assembly: InternalsVisibleTo("Magpie.Tests")]
