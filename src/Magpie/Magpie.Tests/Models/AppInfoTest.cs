1a978ff6e2090ce4a74f6622703487d11eac4db4
Ashok Gelal <ashokgelal@gmail.com>
2017-01-28 11:16:31 -0700

    AppInfo can accept subscribed channel id

---
+++ src/Magpie/Magpie.Tests/Models/AppInfoTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Magpie.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Magpie.Tests.Models
+{
+    [TestClass]
+    public class AppInfoTest
+    {
+        [TestMethod]
+        public void SubscribedChannelIsOneByDefault()
+        {
+            var appInfo = new AppInfo("test_url");
+            Assert.AreEqual(1, appInfo.SubscribedChannel);
+        }
+
+        [TestMethod]
+        public void CanExplicitlySetSubscribedChannel()
+        {
+            var appInfo = new AppInfo("test_url", 2);
+            Assert.AreEqual(2, appInfo.SubscribedChannel);
+        }
+
+        [TestMethod]
+        public void DefaultSignatureFilenameIsSetFromConstructor()
+        {
+            var appInfo = new AppInfo("test_url");
+            Assert.AreEqual(SignatureVerifier.DefaultDSAPubKeyFileName, appInfo.PublicSignatureFilename);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowsExceptionIfAppCastUrlIsNull()
+        {
+            var _ = new AppInfo(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowsExceptionIfAppCastUrlIsEmpty()
+        {
+            var _ = new AppInfo("");
+            var __ = new AppInfo("   ");
+        }
+
+        [TestMethod]
+        public void WhenSettingAppImagePathAppIconPathIsSetToFullPath()
+        {
+            var appInfo = new AppInfo("test_url");
+            appInfo.SetAppIcon("test_namespace", "test_path");
+            Assert.AreEqual("pack://application:,,,/test_namespace;component/test_path", appInfo.AppIconPath);
+        }
+    }
+}
--- src/Magpie/Magpie/Services/AppInfo.cs
+++ src/Magpie/Magpie/Services/AppInfo.cs
@@ -7,20 +7,22 @@ namespace Magpie.Services
         public string AppIconPath { get; set; }
         public string AppCastUrl { get; private set; }
         public string PublicSignatureFilename { get; set; }
+        public int SubscribedChannel { get; set; }
 
         public void SetAppIcon(string imageNamespace, string imagePath)
         {
             AppIconPath = string.Format("pack://application:,,,/{0};component/{1}", imageNamespace, imagePath);
-            PublicSignatureFilename = SignatureVerifier.DefaultDSAPubKeyFileName;
         }
 
-        public AppInfo(string appCastUrl)
+        public AppInfo(string appCastUrl, int subscribedChannel = 1)
         {
             if (string.IsNullOrWhiteSpace(appCastUrl))
             {
                 throw new ArgumentNullException("appCastUrl");
             }
             AppCastUrl = appCastUrl;
+            SubscribedChannel = subscribedChannel;
+            PublicSignatureFilename = SignatureVerifier.DefaultDSAPubKeyFileName;
         }
     }
 }
