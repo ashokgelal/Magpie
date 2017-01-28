3a048ca3292628573f6380486658543b8a4ed9c0
Ashok Gelal <ashokgelal@gmail.com>
2017-01-28 10:53:38 -0700

    Add a Channel model and modify RemoteAppCast to support list of Channel.

--- src/Magpie/Magpie/Models/RemoteAppcast.cs
+++ src/Magpie/Magpie/Models/RemoteAppcast.cs
@@ -1,76 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
 
 namespace Magpie.Models
 {
     [DataContract]
     public class RemoteAppcast
     {
-        [DataMember(Name = "version", IsRequired = true)]
-        private string _version;
-        public Version Version { get; protected set; }
+        [DataMember(Name = "channels", IsRequired = true)]
+        public List<Channel> Channels { get; private set; }
+
         public Dictionary<string, object> RawDictionary { get; internal set; }
 
-        // Dates example:
-        // e.g. January 30, 2015 18:15:00 +0200
-        // 10/03/2015
-        // 2015-10-03
-        [DataMember(Name = "build_date", IsRequired = false)]
-        private string _buildDate;
-        public DateTime BuildDate { get; private set; }
-
-        [DataMember(Name = "dsa_signature", IsRequired = false)]
-        public string DSASignature { get; private set; }
-
-        [DataMember(Name = "artifact_url", IsRequired = true)]
-        public string ArtifactUrl { get; private set; }
-
-        [DataMember(Name = "release_notes_url", IsRequired = true)]
-        public string ReleaseNotesUrl { get; private set; }
-
-        [DataMember(Name = "title", IsRequired = true)]
-        public string Title { get; private set; }
-
-        [OnDeserialized]
-        private void SetValuesOnDeserialized(StreamingContext context)
+        public static RemoteAppcast MakeFromJson(string json)
         {
-            ParseVersion();
-            ParseBuildDate();
-        }
+            try
+            {
+                using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+                {
+                    var settings = new DataContractJsonSerializerSettings {UseSimpleDictionaryFormat = true};
+                    var serializer = new DataContractJsonSerializer(typeof(RemoteAppcast), settings);
+                    var appcast = (RemoteAppcast) serializer.ReadObject(ms);
 
-        private void ParseVersion()
-        {
-            Version version;
-            if (Version.TryParse(_version, out version))
-            {
-                Version = version;
+                    ms.Seek(0, SeekOrigin.Begin);
+                    serializer = new DataContractJsonSerializer(typeof(Dictionary<string, object>), settings);
+                    var results = (Dictionary<string, object>) serializer.ReadObject(ms);
+                    appcast.RawDictionary = results;
+                    return appcast;
+                }
             }
-            else
+            catch (SerializationException)
             {
-                Trace.TraceError("Magpie: Error parsing version");
+                return new RemoteAppcast()
+                {
+                    Channels = new List<Channel>(),
+                    RawDictionary = new Dictionary<string, object>()
+                };
             }
-        }
-
-        private void ParseBuildDate()
-        {
-            if (string.IsNullOrWhiteSpace(_buildDate)) return;
-            DateTime buildDate;
-            if (DateTime.TryParse(_buildDate, CultureInfo.InvariantCulture, DateTimeStyles.AllowWhiteSpaces, out buildDate))
-            {
-                BuildDate = buildDate;
-            }
-            else
-            {
-                Trace.TraceError("Magpie: Error parsing build date");
-            }
-        }
-
-        public override string ToString()
-        {
-            return string.Format("{0} ({1}) Build Date: {2}", Title, Version, BuildDate);
         }
     }
 }
