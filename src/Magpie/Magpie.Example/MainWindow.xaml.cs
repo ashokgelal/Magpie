af33cd1333f1fa1dbd3998707ebff56b161e60f1
Ashok Gelal <ashokgelal@gmail.com>
2017-01-28 20:18:25 -0700

    Update example app to show the latest multi-channel features of Magpie.

--- src/Magpie/Magpie.Example/MainWindow.xaml.cs
+++ src/Magpie/Magpie.Example/MainWindow.xaml.cs
@@ -1,26 +1,61 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 using Magpie.Services;
 
 namespace Magpie.Example
 {
     public partial class MainWindow
     {
+        private int SelectedChannel
+        {
+            get { return _selectedChannel; }
+            set
+            {
+                _selectedChannel = value;
+                SubscribedTo.Content = "Subscribed to Channel ID: " + value;
+            }
+        }
+
+        private Services.MagpieUpdater _magpieUpdater;
+        private int _selectedChannel;
+
         public MainWindow()
         {
             InitializeComponent();
-            new MagpieUpdater(MakeAppInfo()).CheckInBackground();
+            CurrentVersion.Content ="Current version: " + Assembly.GetEntryAssembly().GetName().Version;
+            SelectedChannel = 1;
+            _magpieUpdater = new Services.MagpieUpdater(MakeAppInfo(SelectedChannel));
+            _magpieUpdater.CheckInBackground();
         }
 
         private void ForceCheck_OnClick(object sender, RoutedEventArgs e)
         {
-            new MagpieUpdater(MakeAppInfo()).ForceCheckInBackground();
+            _magpieUpdater.ForceCheckInBackground();
         }
 
-        private static AppInfo MakeAppInfo()
+        private static AppInfo MakeAppInfo(int id)
         {
-            var appInfo = new AppInfo("https://dl.dropboxusercontent.com/u/83257/Updaters/Magpie/appcast.json");
+            var appInfo = new AppInfo("https://dl.dropboxusercontent.com/u/83257/Updaters/Magpie/appcast.json", id);
             appInfo.SetAppIcon("Magpie.Example", "logo64x64.tiff");
             return appInfo;
+        }
+
+        private void BetaChannel_OnClick(object sender, RoutedEventArgs e)
+        {
+            SelectedChannel = 2;
+            _magpieUpdater.SwitchSubscribedChannel(SelectedChannel);
+        }
+
+        private void AlphaChannel_OnClick(object sender, RoutedEventArgs e)
+        {
+            SelectedChannel = 3;
+            _magpieUpdater.SwitchSubscribedChannel(SelectedChannel);
+        }
+
+        private void DailyBuildChannel_OnClick(object sender, RoutedEventArgs e)
+        {
+            SelectedChannel = 4;
+            _magpieUpdater.SwitchSubscribedChannel(SelectedChannel);
         }
     }
 }
